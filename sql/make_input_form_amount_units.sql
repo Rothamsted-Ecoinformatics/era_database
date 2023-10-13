122468040b0ae4c93ff38f02ab74c2a3187a65e8
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2023-10-13 18:17:59 +0100

    Update make_input_form_amount_units.sql

--- sql/make_input_form_amount_units.sql
+++ sql/make_input_form_amount_units.sql
@@ -1,20 +1,14 @@
 create table lte_data.input_form_amount_units (
 	id smallserial not null,
 	symbol text not null,
-	class_label text not null,
-	class_uri text null,
+	term_label text not null,
+	term_uri text null,
 	unit_system text not null,
-	constraint input_form_amount_units_pk primary key(id)
+	constraint input_form_amount_units_pk primary key(id),
+	constraint input_form_amount_units_unq unique (symbol, term_uri)
 );
 
-comment on table lte_data.input_form_amount_units is 'input_form_amount_units is a dimension table of input form amount units. 
-these are typically area density units.';
+comment on table lte_data.input_form_amount_units is 'input_form_amount_units is a dimension table used to provide a standardised list of units for 
+input form amount units. These are typically area density units.';
 
-comment on column lte_data.input_form_amount_units.unit_system is 'indicates whether the unit is metric or imperial.';
-
-insert into lte_data.input_form_amount_units(symbol, class_label, unit_system, class_uri) values 
-	('kg ha-1','kilograms per hectare','metric','http://purl.obolibrary.org/obo/UO_0000283'),
-	('t ha-1','ton per hectare','metric','http://purl.obolibrary.org/obo/UO_0000323'),
-	('cwt acre-1','hundredweights per acre','imperial',null),
-	('ton acre-1','ton per acre','imperial',null),
-	('lb acre-1','lb per acre','imperial',null);
+comment on column lte_data.input_form_amount_units.unit_system is 'Indicates whether the unit is metric or imperial.';
