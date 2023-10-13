f46d5f93d7ca8c01b6e16f241f610a94b5678db5
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2023-10-13 18:20:18 +0100

    Update make_experiment_factors.sql

--- sql/make_experiment_factors.sql
+++ sql/make_experiment_factors.sql
@@ -1,12 +1,12 @@
 create table lte_data.experiment_factors (
 	id smallserial not null,
 	experiment_id smallint not null,
-	factor_annotation_id smallint not null,
+	factor_term_id smallint not null,
 	factor_name text not null,
-	notes text null,
+	description text null,
 	constraint experiment_factors_pk primary key(id),
 	constraint experiment_factors_experiment_id_fk foreign key (experiment_id) references experiments(id),
-	constraint experiment_factors_factor_annotation_id_fk foreign key (factor_annotation_id) references factor_annotations(id)
+	constraint experiment_factors_factor_term_id_fk foreign key (factor_term_id) references factor_terms(id)
 );
 
 comment on table lte_data.experiment_factors is 'experiment_factors is used to identify the specific factors used for an experiment.';
