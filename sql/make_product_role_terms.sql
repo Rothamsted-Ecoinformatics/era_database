5821d6ec988e148cf1604a17690134ad5d6824b1
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2023-11-03 15:25:30 +0000

    Update make_product_role_terms.sql
    Grammar correction to comments

--- sql/make_product_role_terms.sql
+++ sql/make_product_role_terms.sql
@@ -10,5 +10,5 @@ create table lte_data.product_role_terms (
 	constraint product_role_terms_term_uri_unq unique (term_uri)
 );
 
-comment on table lte_data.product_role_terms is 'product_role_terms dimension table used to provide a standardised classification for product roles backed by a curated ontology term, if one exists.';
+comment on table lte_data.product_role_terms is 'product_role_terms dimension table is used to provide a standardised classification for product roles backed by a curated ontology term, if one exists.';
     
