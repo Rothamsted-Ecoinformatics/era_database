74048160392d2361631422a25b768c03185ffb0f
Richard Ostler <richard.ostler@rothamsted.ac.uk>
2023-11-06 14:50:13 +0000

    Update make_dataset_metadata_items.sql

--- sql/make_dataset_metadata_items.sql
+++ sql/make_dataset_metadata_items.sql
@@ -9,8 +9,12 @@ create table dataset_metadata_items (
 	constraint dataset_metadata_items_dataset_id_fk foreign key (dataset_id) references datasets(id)
 );
 
-comment on table lte_data.dataset_metadata_items is 'dataset_metadata_items is used to record additional descipritve metadata for a dataset.
-The table uses a flexible name value pair format, and ideally item labels and values should be annotated with URIs if available. These 
+comment on table lte_data.dataset_metadata_items is 'dataset_metadata_items is used to record additional descriptive metadata for a dataset.
+The table uses a flexible name value pair format, and, if available, item labels and values should be annotated with URIs. These 
 URIs do not currently have an FK relationhship to associated terms tables but this could be added later.';
 
-comment on column lte_data.dataset_metadata_terms.dataset_id is 'FK to the dataset the metadata term is for.';
+comment on column lte_data.dataset_metadata_items.dataset_id is 'dataset_id is an FK to the dataset the metadata item is for.';
+comment on column lte_data.dataset_metadata_items.item_label is 'item_label is the label for the metadata item.';
+comment on column lte_data.dataset_metadata_items.item_value is 'item_value is the value for the metadata item.';
+comment on column lte_data.dataset_metadata_items.item_label_uri is 'item_label_uri is the URI for the label for the metadata item.';
+comment on column lte_data.dataset_metadata_items.item_value_uri is 'item_value_uri is the URI for the value for the metadata item.';
