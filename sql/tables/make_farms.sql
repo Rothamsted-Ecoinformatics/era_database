27998cf824d2cf581897839ff8d060fe8c573261
Nathalie Castells <nathalie.castells@rothamsted.ac.uk>
2026-03-12 16:58:01 +0000

    remove my change on farms

--- sql/tables/make_farms.sql
+++ sql/tables/make_farms.sql
@@ -2,12 +2,11 @@
 CREATE TABLE lte_data.farms (
 	id char(1) NOT NULL,
 	farm_name text not null,
-	organisation_id int2 null,
+	organisation text NOT null,
 	street_address text NOT NULL,
 	address_locality text NOT NULL,
 	address_region text NOT NULL,
 	address_country text NOT NULL,
 	postal_code text NOT NULL,
 	constraint farms_pk primary key (id),
-	constraint farms_organisation_id_fk foreign key (organisation_id) references lte_data.organisations(id)
 );
